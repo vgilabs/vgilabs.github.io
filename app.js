e65f203f4f4b90f74572b0947aa3c5569a728ecb
wonndrive <nonyok812@gmail.com>
2025-02-12 03:38:50 +0900

    Update All

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ window.addEventListener("scroll", function() {
 
     
     var header = document.getElementById("header");
-    var welcome = document.getElementsByClassName("welcome")[0];
+    // var welcome = document.getElementsByClassName("welcome")[0];
     var logo = document.querySelector(".img-logo");
     var banner = document.querySelector(".banner-area");
     var scrollPosition = window.scrollY;
@@ -35,16 +35,60 @@ window.addEventListener("scroll", function() {
     if (window.scrollY > 5) {  // 스크롤되면 상태 변경
         header.classList.add("scrolled");  
         header.classList.remove("collapsed");
-        welcome.classList.add("with-fixed-header");
+        // welcome.classList.add("with-fixed-header");
         logo.src = "../images/logo/ver2/logo_white.png";  // 
     } else {  // 스크롤이 맨 위로 올라가면 원래대로 복귀
         header.classList.remove("scrolled");  
         header.classList.add("collapsed");
-        welcome.classList.remove("with-fixed-header");
+        // welcome.classList.remove("with-fixed-header");
         logo.src = "../images/logo/ver2/left_logo.png"; 
         
         if (window.innerWidth <= 1020) return;
-        banner.style.backgroundPosition = `center calc(-400px)`;
+        // banner.style.backgroundPosition = `center calc(-400px)`;
     }
 });
 
+
+
+var logo = document.querySelector(".img-logo");
+const navLinks_li = document.querySelectorAll('.nav-links li');
+const navLinks_li_a = document.querySelectorAll('.nav-links li a');
+const style1_li = document.querySelectorAll('.Style1 li'); // .Style1 내부의 li 선택
+// const welcome = document.querySelectorAll('.welcome');
+const header = document.querySelector("#header");
+
+
+function handleMouseEnter() {
+  if (window.innerWidth > 1020 && !header.classList.contains('scrolled')) {
+    logo.src = "../images/logo/ver2/logo_white.png";
+    header.classList.add('scrolled');
+  }
+}
+
+function handleMouseLeave() {
+  if (window.innerWidth > 1020 && header.classList.contains('scrolled') && window.scrollY <= 5) {
+    logo.src = "../images/logo/ver2/left_logo.png";
+    header.classList.remove('scrolled');
+  }
+}
+
+
+navLinks_li.forEach(link => {
+  link.addEventListener('mouseenter', handleMouseEnter);
+  link.addEventListener('mouseleave', handleMouseLeave);
+});
+
+style1_li.forEach(link => {
+  link.addEventListener('mouseenter', handleMouseEnter);
+  link.addEventListener('mouseleave', handleMouseLeave);
+});
+
+navLinks_li_a.forEach(link => {
+    link.addEventListener('mouseenter', handleMouseEnter);
+    link.addEventListener('mouseleave', handleMouseLeave);
+});
+
+// welcome.forEach(link => {
+//     link.addEventListener('mouseenter', handleMouseEnter);
+//     link.addEventListener('mouseleave', handleMouseLeave);
+// });
--- app_members.js
+++ app_members.js
@@ -20,12 +20,11 @@ function show() {
 }
 
 
-
 window.addEventListener("scroll", function() { 
 
     
     var header = document.getElementById("header");
-    var welcome = document.getElementsByClassName("welcome")[0];
+    // var welcome = document.getElementsByClassName("welcome")[0];
     var logo = document.querySelector(".img-logo");
     var banner = document.querySelector(".banner-area");
     var scrollPosition = window.scrollY;
@@ -35,12 +34,12 @@ window.addEventListener("scroll", function() {
     if (window.scrollY > 5) {  // 스크롤되면 상태 변경
         header.classList.add("scrolled");  
         header.classList.remove("collapsed");
-        welcome.classList.add("with-fixed-header");
+        // welcome.classList.add("with-fixed-header");
         logo.src = "../../images/logo/ver2/logo_white.png";  // 
     } else {  // 스크롤이 맨 위로 올라가면 원래대로 복귀
         header.classList.remove("scrolled");  
         header.classList.add("collapsed");
-        welcome.classList.remove("with-fixed-header");
+        // welcome.classList.remove("with-fixed-header");
         logo.src = "../../images/logo/ver2/left_logo.png"; 
         
         if (window.innerWidth <= 1020) return;
@@ -48,3 +47,35 @@ window.addEventListener("scroll", function() {
     }
 });
 
+
+
+var logo = document.querySelector(".img-logo");
+const navLinks_li = document.querySelectorAll('.nav-links li');
+const style1_li = document.querySelectorAll('.Style1 li'); // .Style1 내부의 li 선택
+const header = document.querySelector("#header");
+
+// 공통 동작 함수
+function handleMouseEnter() {
+  if (window.innerWidth > 1020 && !header.classList.contains('scrolled')) {
+    logo.src = "../../images/logo/ver2/logo_white.png";
+    header.classList.add('scrolled');
+  }
+}
+
+function handleMouseLeave() {
+  if (window.innerWidth > 1020 && header.classList.contains('scrolled') && window.scrollY <= 5) {
+    logo.src = "../../images/logo/ver2/left_logo.png";
+    header.classList.remove('scrolled');
+  }
+}
+
+
+navLinks_li.forEach(link => {
+  link.addEventListener('mouseenter', handleMouseEnter);
+  link.addEventListener('mouseleave', handleMouseLeave);
+});
+
+style1_li.forEach(link => {
+  link.addEventListener('mouseenter', handleMouseEnter);
+  link.addEventListener('mouseleave', handleMouseLeave);
+});
