20b5d2c742f1e4e38c3622bcf7cfaa3485b956d7
wonndrive <nonyok812@gmail.com>
2025-09-10 16:06:15 +0900

    Update news and gallery

--- gallery.js
+++ gallery.js
@@ -1,20 +1,25 @@
-let indexes = {Seminar_2025: 0, KCCV_2025: 0, ICML_2025: 0, CVPR_2025: 0, KCCV_2024: 0, CVPR_2024: 0 , KCCV_2023: 0}; 
+let indexes = {Seminar_2025: 0, KCCV_2025: 0, ICML_2025: 0, CVPR_2025: 0, KCCV_2024: 0, ECCV_2024: 0, CVPR_2024: 0, KCCV_2023: 0}; 
+// let indexes = {Seminar_2025: 0, KCCV_2025: 0, ICML_2025: 0, CVPR_2025: 0, ECCV_2024: 0, KCCV_2024: 0, CVPR_2024: 0, ICCV_2024: 0, KCCV_2023: 0}; 
 
 document.addEventListener("DOMContentLoaded", function () {
     initDots("Seminar_2025");
     initDots("KCCV_2025");
     initDots("ICML_2025");
     initDots("CVPR_2025");
+    initDots("ECCV_2024");
     initDots("KCCV_2024");
     initDots("CVPR_2024");
+    // initDots("ICCV_2024");
     initDots("KCCV_2023");
 
     updateSlide("Seminar_2025");
     updateSlide("KCCV_2025");
     updateSlide("ICML_2025");
     updateSlide("CVPR_2025");
+    updateSlide("ECCV_2024");
     updateSlide("KCCV_2024");
     updateSlide("CVPR_2024");
+    // updateSlide("ICCV_2024");
     updateSlide("KCCV_2023");
 });
 
