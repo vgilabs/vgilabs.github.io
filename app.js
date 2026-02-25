cab13e19c67137041a4e7d2526888b8e0ca91502
wonndrive <nonyok812@gmail.com>
2026-02-25 18:00:41 +0900

    Update patents

---
+++ app.js
@@ -0,0 +1,94 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', show);
+
+function show() {
+    //Burger Animation
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('close');
+    
+    //Animate Links
+    for (var index = 0; index < navLinks.length; index++) {
+        if(navLinks[index].style.animation){
+            navLinks[index].style.animation = '';
+        } else {
+            navLinks[index].style.animation = 'navLinkFade 0.3s ease forwards';
+        }
+    };
+}
+
+
+
+window.addEventListener("scroll", function() { 
+
+    
+    var header = document.getElementById("header");
+    // var welcome = document.getElementsByClassName("welcome")[0];
+    var logo = document.querySelector(".img-logo");
+    var banner = document.querySelector(".banner-area");
+    var scrollPosition = window.scrollY;
+
+    banner.style.backgroundPosition = `center ${50 - scrollPosition * 0.05}%`;
+
+    if (window.scrollY > 5) {  // 스크롤되면 상태 변경
+        header.classList.add("scrolled");  
+        header.classList.remove("collapsed");
+        // welcome.classList.add("with-fixed-header");
+        logo.src = "../images/logo/ver2/logo_white.png";  // 
+    } else {  // 스크롤이 맨 위로 올라가면 원래대로 복귀
+        header.classList.remove("scrolled");  
+        header.classList.add("collapsed");
+        // welcome.classList.remove("with-fixed-header");
+        logo.src = "../images/logo/ver2/left_logo.png"; 
+        
+        if (window.innerWidth <= 1020) return;
+        // banner.style.backgroundPosition = `center calc(-400px)`;
+    }
+});
+
+
+
+var logo = document.querySelector(".img-logo");
+const navLinks_li = document.querySelectorAll('.nav-links li');
+const navLinks_li_a = document.querySelectorAll('.nav-links li a');
+const style1_li = document.querySelectorAll('.Style1 li'); // .Style1 내부의 li 선택
+// const welcome = document.querySelectorAll('.welcome');
+const header = document.querySelector("#header");
+
+
+function handleMouseEnter() {
+  if (window.innerWidth > 1020 && !header.classList.contains('scrolled')) {
+    logo.src = "../images/logo/ver2/logo_white.png";
+    header.classList.add('scrolled');
+  }
+}
+
+function handleMouseLeave() {
+  if (window.innerWidth > 1020 && header.classList.contains('scrolled') && window.scrollY <= 5) {
+    logo.src = "../images/logo/ver2/left_logo.png";
+    header.classList.remove('scrolled');
+  }
+}
+
+
+navLinks_li.forEach(link => {
+  link.addEventListener('mouseenter', handleMouseEnter);
+  link.addEventListener('mouseleave', handleMouseLeave);
+});
+
+style1_li.forEach(link => {
+  link.addEventListener('mouseenter', handleMouseEnter);
+  link.addEventListener('mouseleave', handleMouseLeave);
+});
+
+navLinks_li_a.forEach(link => {
+    link.addEventListener('mouseenter', handleMouseEnter);
+    link.addEventListener('mouseleave', handleMouseLeave);
+});
+
+// welcome.forEach(link => {
+//     link.addEventListener('mouseenter', handleMouseEnter);
+//     link.addEventListener('mouseleave', handleMouseLeave);
+// });
---
+++ gallery.js
@@ -0,0 +1,65 @@
+let indexes = { NeurIPS_2025: 0, ICCV_2025: 0, Seminar_2025: 0, KCCV_2025: 0, ICML_2025: 0, CVPR_2025: 0, ECCV_2024: 0, KCCV_2024: 0, CVPR_2024: 0, KCCV_2023: 0 };
+// let indexes = { ICCV_2025: 0, Seminar_2025: 0, KCCV_2025: 0, ICML_2025: 0, CVPR_2025: 0, ECCV_2024: 0, KCCV_2024: 0, CVPR_2024: 0, ICCV_2023: 0, KCCV_2023: 0 };
+
+document.addEventListener("DOMContentLoaded", function () { 
+    initDots("NeurIPS_2025");
+    initDots("ICCV_2025");
+    initDots("Seminar_2025");
+    initDots("KCCV_2025");
+    initDots("ICML_2025");
+    initDots("CVPR_2025");
+    initDots("ECCV_2024");
+    initDots("KCCV_2024");
+    initDots("CVPR_2024");
+    // initDots("ICCV_2023");
+    initDots("KCCV_2023");
+
+    updateSlide("NeurIPS_2025");
+    updateSlide("ICCV_2025");
+    updateSlide("Seminar_2025");
+    updateSlide("KCCV_2025");
+    updateSlide("ICML_2025");
+    updateSlide("CVPR_2025");
+    updateSlide("ECCV_2024");
+    updateSlide("KCCV_2024");
+    updateSlide("CVPR_2024");
+    // updateSlide("ICCV_2023");
+    updateSlide("KCCV_2023");
+});
+
+function initDots(gallery) {
+    const slides = document.querySelector(`#${gallery}-gallery .slides`).children;
+    const dotsContainer = document.querySelector(`#${gallery}-gallery .dots-container`);
+    dotsContainer.innerHTML = "";
+
+    Array.from(slides).forEach((_, i) => {
+        let dot = document.createElement("span");
+        dot.classList.add("dot");
+        dot.addEventListener("click", () => currentSlide(i, gallery));
+        dotsContainer.appendChild(dot);
+    });
+}
+
+function moveSlide(step, gallery) {
+    const slides = document.querySelector(`#${gallery}-gallery .slides`).children;
+    indexes[gallery] = (indexes[gallery] + step + slides.length) % slides.length;
+    updateSlide(gallery);
+}
+
+function currentSlide(n, gallery) {
+    indexes[gallery] = n;
+    updateSlide(gallery);
+}
+
+function updateSlide(gallery) {
+    const slides = document.querySelectorAll(`#${gallery}-gallery .slide`);
+    const dots = document.querySelectorAll(`#${gallery}-gallery .dot`);
+
+    slides.forEach((slide, i) => {
+        slide.style.display = i === indexes[gallery] ? "block" : "none";
+    });
+
+    dots.forEach((dot, i) => {
+        dot.classList.toggle("active", i === indexes[gallery]);
+    });
+}
