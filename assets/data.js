03f7a4f46df8e034d5c83566a966ff152f7634f4
wonndrive <nonyok812@gmail.com>
2026-08-07 23:51:50 +0900

    Add news

--- assets/data.js
+++ assets/data.js
@@ -104,6 +104,9 @@ window.VGI_DATA = {
   ],
   // Add new news items at the top, newest date first.
   news: [
+    { date: "2026-08-03", html: "VGILAB receives a research grant funded by IITP under the AI Star Fellowship (<b class='accent'>AI최고급신진연구자지원</b>) program." },
+    { date: "2026-08-03", html: "Prof. Gyeong-Moon Park will serve as an Academic Committee Member of <a href='https://www.kosaim.org/html/?pmode=inputList&smode=view&part=&intAcSeq=109' target='_blank' rel='noopener noreferrer' class='news-link'><b><i>KoSAIM 2026</i></b></a>." },
+    { date: "2026-08-03", html: "Prof. Gyeong-Moon Park will serve as a Registration Chair for <a href='https://kcvs.kr/?act=info.workshop&pseq=9' target='_blank' rel='noopener noreferrer' class='news-link'><b><i>KCCV 2026</i></b></a>." },
     { date: "2026-07-22", html: "Prof. Gyeong-Moon Park will give a talk titled “Perturb a Model, Not an Image: Towards Robust Privacy Protection via Anti-Personalized Diffusion Models” at <a href='https://kcvs.kr/?act=info.workshop&pcode=schedule_2026&pseq=9' target='_blank' rel='noopener noreferrer' class='news-link'><b><i>KCCV 2026</i></b></a>." },
     { date: "2026-07-22", html: "Prof. Gyeong-Moon Park will give a talk titled “Towards Privacy-Preserving Generative Intelligence” at <a href='https://aisociety.kr/ai-korea-2026/program' target='_blank' rel='noopener noreferrer' class='news-link'><b><i>THE AI KOREA 2026</i></b></a>." },
     { date: "2026-06-18", html: "<b class='accent'>Four papers</b> were accepted to <b><i>ECCV 2026</i></b>." },
